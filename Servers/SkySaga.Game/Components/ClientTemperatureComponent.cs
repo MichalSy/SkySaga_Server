8db68016d24123a7898f083903be388c29fc50ef
Michal Sytko <michal.sytko@gmail.lcom>
2025-12-27 20:43:32 +0100

    added ClientTemperatureComponent

---
+++ Servers/SkySaga.Game/Components/ClientTemperatureComponent.cs
@@ -0,0 +1,31 @@
+﻿namespace SkySaga.Game.Components;
+
+public class ClientTemperatureComponent : Component
+{
+    public byte TemperatureState { get; set => SetIfChanged(ref field, value); } = 1;
+    public byte MinTemperature { get; set => SetIfChanged(ref field, value); } = 0;
+    public byte MaxTemperature { get; set => SetIfChanged(ref field, value); } = 200;
+
+    public override bool TrySync(string parameterName, BitStream bitStream)
+    {
+        if (parameterName.Equals(nameof(TemperatureState), StringComparison.OrdinalIgnoreCase))
+        {
+            bitStream.WriteBits([TemperatureState], 8 - Util.NumBitsRequiredByte(3), true);
+
+            return true;
+        }
+        else if (parameterName.Equals(nameof(MinTemperature), StringComparison.OrdinalIgnoreCase))
+        {
+            bitStream.WriteBits([MinTemperature], 8 - Util.NumBitsRequiredByte(200), true);
+
+            return true;
+        }
+        else if (parameterName.Equals(nameof(MaxTemperature), StringComparison.OrdinalIgnoreCase))
+        {
+            bitStream.WriteBits([MaxTemperature], 8 - Util.NumBitsRequiredByte(200), true);
+
+            return true;
+        }
+        return false;
+    }
+}
